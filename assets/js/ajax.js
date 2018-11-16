2659eaf30c3dd4eceebb65f6784490451ff5c488
dragan <dragovanmarkic@gmail.com>
2018-11-16 13:55:29 +0100

    ajax request complete

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,94 +1,67 @@
-
 //Create the XHR Object
-let httpreq = new XMLHttpRequest;
+let httpreq = new XMLHttpRequest();
 let articles = document.querySelector(".articles");
 console.log(articles);
 
-
-
-    //Call the open function, GET-type of request, url, true-asynchronous
-httpreq.open('GET', 'http://foodog.herokuapp.com/articles', true)
-    //call the onload 
+//Call the open function, GET-type of request, url, true-asynchronous
+httpreq.open("GET", "http://foodog.herokuapp.com/articles", true);
+//call the onload
 httpreq.onload = function() {
-        //check if the status is 200(means everything is okay)
-        if (this.status === 200) {
-            //return server response as an object with JSON.parse
-            console.log("200");
+  //check if the status is 200(means everything is okay)
+  if (this.status === 200) {
+    //return server response as an object with JSON.parse
+    console.log("200");
 
-            let data = JSON.parse(this.responseText);
-            
+    let data = JSON.parse(httpreq.responseText);
 
-            for (let i = 0; i < data.docs.length; i++) {
-                let article = document.createElement("article");
-                article.classList.add("article", "columns");
+    for (let i = 0; i < data.docs.length; i++) {
+                              // console.log(data.docs[i].text);
 
-                let img = document.createElement("img");
-                img.classList.add("column", "is-half");
+                              let article = document.createElement("article");
+                              article.classList.add("article", "columns");
 
-                let descr = document.createElement("div");
-                descr.classList.add("description", "column", "is-half");
+                              articles.appendChild(article);
+                              article.innerHTML = /*html*/ `
+                                              <img class="column is-half" src="${data.docs[i].imgUrl}" alt="dogs" />
 
-                let tags = document.createElement("div");
-                tags.classList.add("tags");
+                                              <div class="description column is-half">
+                                                <div id="tags${i}" class="tags"> 
+                                                      <!-- TAGS ARE COMING -->                                   
+                                                </div>
 
-                let meta = document.createElement("div");
-                meta.classList.add("meta");
+                                                <div class="artitle">
+                                                  <a href="single.html" class="uppercase">${data.docs[i].title}</a>
+                                                </div>
 
-                let meta = document.createElement("div");
-                meta.classList.add("meta");
+                                                <p class="extract">${data.docs[i].text.substring(0,200)}...</p>
 
-                
+                                                <a href="#share" class="uppercase">Share</a>
+                                              </div>`;
 
+                              for (j = 0; j < data.docs[i].tagForArticle.length; j++) {
+                                
+                                                                      document.createElement("div").classList.add("meta");
 
+                                                                      document.querySelector(`#tags${i}`).appendChild                    
 
+                                                                      
+                                                                      document.querySelector(`#tags${i}`).innerHTML = /*html*/ `
+                                                                                                    <div class="meta">
+                                                                                                        <div class="underline">
+                                                                                                            <a class="uppercase" href="wellness.html">${data.docs[i].tagForArticle[j]}</a>
+                                                                                              </div>
+                                                                                            </div>`;
+                                                                                              }
+                            }
+  } else {
+    console.log("error");
+    articles.innerText = "Sorry we couldn't reach the server";
+  }
+};
 
-
-
-               
-
-                article.classList.add("article","columns");
-                articles.appendChild(article);
-
-
-                
-
-
-
-
-                
-            }
-
-            let article = document.createElement("article")
-            article.classList.add
-
-
-
-
-
-           
-
-
-            let auth = document.createElement("h3");
-            block.appendChild(auth);
-            auth.innerText = response.author;
-
-
-
-
-
-            console.log(JSON.parse(this.responseText));
-
-
-
-        } else {
-            console.log("error");
-            block.innerText = "Sorry we couldn't reach the server";
-        }
-    }
-    //call send
+//call send
 httpreq.send();
 //Common Types of HTTP Statuses
 // 200: OK
 // 404: ERROR
 // 403: FORBIDDEN
-
